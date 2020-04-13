70c5da473c9e1b217b951145c6258af6daf5477a
Haoqi Wang <HaoqiWang@users.noreply.github.com>
2020-04-13 17:53:14 -0500

    remove_color function

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # print("Hello world")
 
 # from utilities import read_image
+
+from utilities import remove_color
+
+'''
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 import numpy as np
 
 I = mpimg.imread('natural_forest.jpg')
-
-print(I.shape)
+I=np.asarray(I).astype(np.int32)
+#print(I.shape)
 plt.imshow(I)
+plt.imshow(I,cmap="gray")
+#plt.colorbar()
+#remove red
+I2=I[:,:,0]
+plt.imshow(I2,cmap="gray")
+'''
+from utilities import remove_color
+import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = mpimg.imread('natural_forest.jpg')
+
+image_remove_red = remove_color.remove_red(image)
+plt.show()
