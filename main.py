ad5cc96e32a5614ec878c510bcc67944e7c2c5e3
Haoqi Wang <HaoqiWang@users.noreply.github.com>
2020-04-13 16:29:52 -0500

    some TODO

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+# print("Hello world")
+
+# from utilities import read_image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import numpy as np
+
+I = mpimg.imread('natural_forest.jpg')
+
+print(I.shape)
+plt.imshow(I)
---
+++ utilities/initiate_array.py
@@ -0,0 +1,8 @@
+# TODO: initiate 256*256*10*? array to store the predicted color, which can be multiple
+
+# TODO: initiate 256*256*10 array to store mean of predicted color channel
+
+# TODO: NOT possible to store the value in memory, that is too large, write it in excel or csv
+
+# TODO: an excel for a color, containing 10 sheets, or maybe 20 (another sheet is for the number, to calculate mean)
+#  , each sheet represents a level of contrast, so we just add them and then calculate mean
