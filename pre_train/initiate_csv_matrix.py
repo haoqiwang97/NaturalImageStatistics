1548e7f49a1baa5de008f364458eed3f1e426d75
Haoqi Wang <HaoqiWang@users.noreply.github.com>
2020-04-19 10:03:12 -0500

    formalize plotting

--- pre_train/initiate_csv_matrix.py
+++ pre_train/initiate_csv_matrix.py
@@ -40,65 +40,62 @@ rows: RG_C00_record_G_0, RG_C00_record_G_1, ... RG_C00_record_G_255
 import numpy as np
 import pandas as pd
 
+
 def generate_filename_list(num_contrast):
-    filename_list=[]
+    filename_list = []
     for i in range(num_contrast):
-        RG_filename='RG_C0' + str(i)
-        RB_filename='RB_C0' + str(i)
-        GB_filename='GB_C0' + str(i)
-        RG_record_filename='RG_C0' + str(i)+'_record'
-        RB_record_filename='RB_C0' + str(i)+'_record'
-        GB_record_filename='GB_C0' + str(i)+'_record'
-        
+        RG_filename = 'RG_C0' + str(i)
+        RB_filename = 'RB_C0' + str(i)
+        GB_filename = 'GB_C0' + str(i)
+        RG_record_filename = 'RG_C0' + str(i) + '_record'
+        RB_record_filename = 'RB_C0' + str(i) + '_record'
+        GB_record_filename = 'GB_C0' + str(i) + '_record'
+
         filename_list.append(RG_filename)
         filename_list.append(RG_record_filename)
-        
+
         filename_list.append(RB_filename)
         filename_list.append(RB_record_filename)
-        
+
         filename_list.append(GB_filename)
         filename_list.append(GB_record_filename)
 
     return filename_list
 
+
 def generate_csv(filename):
-    
-    df = pd.DataFrame(np.zeros((num_row,num_column),dtype=np.int32))
-    
+    df = pd.DataFrame(np.zeros((num_row, num_column), dtype=np.int32))
+
     def column_row_name(filename):
-        #generate columns and rows names
-        #generate a 256*256 all 0 matrix
-        column_name_list=np.zeros(num_column,dtype=object)
-        row_name_list=np.zeros(num_row,dtype=object)
+        # generate columns and rows names
+        # generate a 256*256 all 0 matrix
+        column_name_list = np.zeros(num_column, dtype=object)
+        row_name_list = np.zeros(num_row, dtype=object)
         for i in range(num_column):
-            column_name=filename+'_'+filename[0]+'_'+str(i)
-            column_name_list[i]=column_name
-            row_name=filename+'_'+filename[1]+'_'+str(i)
-            row_name_list[i]=row_name
-        return column_name_list,row_name_list
-    
-    column_name_list,row_name_list=column_row_name(filename) 
-    
-    df.columns=column_name_list#set names of columns
-    df.index=row_name_list#set names of rows
+            column_name = filename + '_' + filename[0] + '_' + str(i)
+            column_name_list[i] = column_name
+            row_name = filename + '_' + filename[1] + '_' + str(i)
+            row_name_list[i] = row_name
+        return column_name_list, row_name_list
+
+    column_name_list, row_name_list = column_row_name(filename)
+
+    df.columns = column_name_list  # set names of columns
+    df.index = row_name_list  # set names of rows
     return df
+
 
 filepath = './train_database/'
 
-num_row=256#number of rows
-num_column=256#number of columns
-num_contrast=10#10 levels of contrast
+num_row = 256  # number of rows
+num_column = 256  # number of columns
+num_contrast = 10  # 10 levels of contrast
 
-filename_list=generate_filename_list(num_contrast)
+filename_list = generate_filename_list(num_contrast)
 
 filename = pd.DataFrame(filename_list)
-filename.to_csv(filepath+'filename_list'+'.csv')#store filename_list
+filename.to_csv(filepath + 'filename_list' + '.csv')  # store filename_list
 
 for filename in filename_list:
-    df=generate_csv(filename)
-    df.to_csv(filepath+filename+'.csv')
-
-
-
-
-
+    df = generate_csv(filename)
+    df.to_csv(filepath + filename + '.csv')
--- pre_train/record_visualize.py
+++ pre_train/record_visualize.py
@@ -11,18 +11,120 @@ from lib import image_operation
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import colors
 
 RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix = csv_operation.import_csv()
 
 IMAGE_FOLDER = './image_database/'
-image_name='natural_forest.jpg'
+image_name = 'natural_forest.jpg'
 
-image_path=IMAGE_FOLDER+image_name
+image_path = IMAGE_FOLDER + image_name
 
 image = image_operation.read_image(image_path)
 
 plt.imshow(image)
+# %%
+# plot predicted blue
+num_row = 2
+num_column = 5
+num = 0
+fig, axes = plt.subplots(nrows=num_row, ncols=num_column, figsize=(12.5, 5))
 
+images = []
+for i in range(num_row):
+    for j in range(num_column):
+        img = axes[i, j].imshow(RG_matrix[:, :, num], cmap=plt.cm.Blues)
+        axes[i, j].set_title('$\hat B_{opt}$, $C_{RG}=0.%d$' % (num))
+        axes[i, j].set_xlabel('R')
+        axes[i, j].set_ylabel('G')
+
+        images.append(img)
+        axes[i, j].label_outer()
+        num += 1
+
+# Find the min and max of all colors for use in setting the color scale.
+vmin = min(image.get_array().min() for image in images)
+vmax = max(image.get_array().max() for image in images)
+norm = colors.Normalize(vmin=vmin, vmax=vmax)
+for im in images:
+    im.set_norm(norm)
+
+fig.colorbar(images[0], ax=axes, orientation='vertical', fraction=.05)
+
+# fig.tight_layout()
+
+plt.savefig("natural_forest_predicted_blue", dpi=300)
+
+plt.show()
+# %%
+# plot predicted green
+num_row = 2
+num_column = 5
+num = 0
+fig, axes = plt.subplots(nrows=num_row, ncols=num_column, figsize=(12.5, 5))
+
+images = []
+for i in range(num_row):
+    for j in range(num_column):
+        img = axes[i, j].imshow(RB_matrix[:, :, num], cmap=plt.cm.Greens)
+        axes[i, j].set_title('$\hat G_{opt}$, $C_{RB}=0.%d$' % (num))
+        axes[i, j].set_xlabel('R')
+        axes[i, j].set_ylabel('B')
+
+        images.append(img)
+        axes[i, j].label_outer()
+        num += 1
+
+# Find the min and max of all colors for use in setting the color scale.
+vmin = min(image.get_array().min() for image in images)
+vmax = max(image.get_array().max() for image in images)
+norm = colors.Normalize(vmin=vmin, vmax=vmax)
+for im in images:
+    im.set_norm(norm)
+
+fig.colorbar(images[0], ax=axes, orientation='vertical', fraction=.05)
+
+# fig.tight_layout()
+
+plt.savefig("natural_forest_predicted_green", dpi=300)
+
+plt.show()
+# %%
+# plot predicted red
+num_row = 2
+num_column = 5
+num = 0
+fig, axes = plt.subplots(nrows=num_row, ncols=num_column, figsize=(12.5, 5))
+# fig.suptitle('')
+
+images = []
+for i in range(num_row):
+    for j in range(num_column):
+        img = axes[i, j].imshow(GB_matrix[:, :, num], cmap=plt.cm.Reds)
+        axes[i, j].set_title('$\hat R_{opt}$, $C_{GB}=0.%d$' % (num))
+        axes[i, j].set_xlabel('G')
+        axes[i, j].set_ylabel('B')
+
+        images.append(img)
+        axes[i, j].label_outer()
+        num += 1
+
+# Find the min and max of all colors for use in setting the color scale.
+vmin = min(image.get_array().min() for image in images)
+vmax = max(image.get_array().max() for image in images)
+norm = colors.Normalize(vmin=vmin, vmax=vmax)
+for im in images:
+    im.set_norm(norm)
+
+fig.colorbar(images[0], ax=axes, orientation='vertical', fraction=.05)
+
+# fig.tight_layout()
+
+# plt.savefig("natural_forest_predicted_red",dpi=300)
+
+plt.show()
+# %%
+# trail
 RG_C00 = RG_matrix[:, :, 0]
 plt.imshow(RG_matrix[:, :, 0], cmap=plt.cm.Blues)
 plt.colorbar()
@@ -35,6 +137,7 @@ GB_C00 = GB_matrix[:, :, 0]
 plt.imshow(GB_matrix[:, :, 0], cmap=plt.cm.Reds)
 plt.colorbar()
 
+# %%
 num_channel = 3
 num_row = 256
 # If we combine them
