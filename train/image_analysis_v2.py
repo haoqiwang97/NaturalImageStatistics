ef10822938c8ded313523b338be684b3ceb2998a
Haoqi Wang <HaoqiWang@users.noreply.github.com>
2020-05-06 13:37:56 -0500

    images trained!

---
+++ train/image_analysis_v2.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 22:57:18 2020
+
+@author: haoqiwang
+"""
+
+'''
+2nd version of image_analysis
+I will try to write that opening csv for each image to help speed up
+'''
+import os
+import datetime
+
+from lib import csv_operation
+from lib import pixel_operation
+from lib import image_operation
+
+#IMAGE_FOLDER = './image_database/'  # image location
+#IMAGE_FOLDER = 'F:\\image_database\\train_temp\\'
+#IMAGE_FOLDER = 'F:\\image_database\\cps20100503.ppmprocessed\\'
+#IMAGE_FOLDER = 'F:\\image_database\\cps20100428.ppmprocessed\\'
+#IMAGE_FOLDER = 'F:\\image_database\\cps20100429.ppmprocessed\\'
+#IMAGE_FOLDER = 'F:\\image_database\\cps20100430.ppmprocessed\\'
+#IMAGE_FOLDER = 'F:\\image_database\\cps20100502.ppmprocessed\\'
+#IMAGE_FOLDER = 'F:\\image_database\\cps20100511.ppmprocessed\\'
+#IMAGE_FOLDER = 'F:\\image_database\\cps20100513.ppmprocessed\\'
+#IMAGE_FOLDER = 'F:\\image_database\\cps20101008.ppmprocessed\\'
+IMAGE_FOLDER = 'F:\\image_database\\cps20101012.ppmprocessed\\'
+
+# get names for all images
+image_name_list = [name for name in os.listdir(IMAGE_FOLDER)]
+# image_index=0
+for image_index in range(len(image_name_list)):
+    image_name = image_name_list[image_index]
+    image_path = IMAGE_FOLDER + image_name
+    # start stopwatch
+    t1 = datetime.datetime.now()
+
+    RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix = csv_operation.import_csv()
+
+    image = image_operation.read_image(image_path)
+
+    num_point, RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix = pixel_operation.pixel_analysis(image, RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix)
+
+    csv_operation.export_csv(RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix)
+
+    t2 = datetime.datetime.now()
+
+    time_elapse = t2 - t1
+
+    print('The number of image: ' + str(image_index))
+    print('Image name: ' + image_name)
+    print('time elapse =', time_elapse)
+    print('\n')
+
+    file = open("./record/test.txt", "a")
+
+    file.writelines('Date time: ' + str(datetime.datetime.now()))
+    file.writelines('\n')
+
+    file.writelines('Folder time: ' + IMAGE_FOLDER)
+    file.writelines('\n')
+    
+    file.writelines('@jit(nopython=True,nogil=True)')
+    file.writelines('\n')
+
+    file.writelines('The number of image: ' + str(image_index))
+    file.writelines('\n')
+    file.writelines('Image name: ' + image_name)
+    file.writelines('\n')
+    file.writelines('Image dimension: ' + str(image.shape[0]) + '*' + str(image.shape[1]))
+    file.writelines('\n')
+    file.writelines('Total number of pixels: ' + str(image.shape[0] * image.shape[1]))
+    file.writelines('\n')
+    file.writelines('Total number of trained points: ' + str(num_point))
+    file.writelines('\n')
+    file.writelines('time elapse =' + str(time_elapse))
+    file.writelines('\n')
+    file.writelines('\n')
+
+    file.close()
---
+++ train/lib/pixel_operation.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 10:42:08 2020
+
+@author: haoqiwang
+"""
+
+import numpy as np
+
+from numba import jit
+
+'''
+
+calculate contrast of one pixel and its surrending pixels
+
+'''
+
+
+@jit(nopython=True, nogil=True)
+def rms_contrast(image, i, j, channel):
+    p00 = image[i - 1, j - 1, channel]
+    p01 = image[i - 1, j, channel]
+    p02 = image[i - 1, j + 1, channel]
+
+    p10 = image[i, j - 1, channel]
+    p11 = image[i, j, channel]
+    p12 = image[i, j + 1, channel]
+
+    p20 = image[i + 1, j - 1, channel]
+    p21 = image[i + 1, j, channel]
+    p22 = image[i + 1, j + 1, channel]
+
+    # contrast_matrix = [p00, p01, p02, p10, p11, p12, p20, p21, p22]
+
+    contrast_matrix = np.array([p00, p01, p02, p10, p11, p12, p20, p21, p22], dtype=np.float32)
+    
+    if np.mean(contrast_matrix)>0:
+        rms_contrast = np.std(contrast_matrix) / np.mean(contrast_matrix)
+        return rms_contrast
+    else:
+        return 0
+
+'''
+
+the contrast_index is used to locate
+
+'''
+
+
+@jit(nopython=True, nogil=True)
+def contrast_index(contrast_mean):
+    # because little error or approximation during rms_contrast, some contrast value may slightly greater than 1
+    if contrast_mean < 1:
+        return int(contrast_mean * 10)
+    else:
+        return int(9)
+
+
+'''
+
+analyze all pixels except border
+
+Example:
+
+from lib import pixel_operation
+
+num_point, RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix = pixel_operation.pixel_analysis(
+        image, RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix)
+
+'''
+
+
+@jit(nopython=True, nogil=True)
+def pixel_analysis(image, RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix):
+    num_point = 0
+
+    image_size = image.shape
+
+    num_channel = image_size[2]
+    num_row = image_size[0]  # how many rows
+    num_column = image_size[1]  # how many columns
+
+    # i = 1
+    # j = 1
+    # iterate each pixel except border
+    # for i in np.arange(1, num_row - 1):
+    # for j in np.arange(1, num_column - 1):
+    for i in np.arange(1, num_row - 1):
+        for j in np.arange(1, num_column - 1):
+            # print(i, j)
+            # ith row, jth column pixel
+            red_value = image[i, j, 0]
+            green_value = image[i, j, 1]
+            blue_value = image[i, j, 2]
+
+            red_contrast = rms_contrast(image, i, j, 0)
+            green_contrast = rms_contrast(image, i, j, 1)
+            blue_contrast = rms_contrast(image, i, j, 2)
+
+            red_green_contrast_mean = (red_contrast + green_contrast) / 2
+            red_blue_contrast_mean = (red_contrast + blue_contrast) / 2
+            green_blue_contrast_mean = (green_contrast + blue_contrast) / 2
+
+            # red_green_contrast_mean = red_green_contrast_mean.astype(np.float16)
+            # red_blue_contrast_mean = red_blue_contrast_mean.astype(np.float16)
+            # green_blue_contrast_mean = green_blue_contrast_mean.astype(np.float16)
+
+            # RG
+            # RG_matrix_index = int(str(red_green_contrast_mean)[2])#the string makes us cannot apply numba to accelerate
+            # RG_matrix_index = int(red_green_contrast_mean*10)
+            RG_matrix_index = contrast_index(red_green_contrast_mean)
+
+            # extract origianl values
+            RG_matrix_cell = RG_matrix[green_value, red_value, RG_matrix_index]
+            RG_record_matrix_cell = RG_record_matrix[green_value, red_value, RG_matrix_index]
+            # update value
+            RG_matrix[green_value, red_value, RG_matrix_index] = int(
+                (RG_matrix_cell * RG_record_matrix_cell + blue_value) / (RG_record_matrix_cell + 1))
+            RG_record_matrix[green_value, red_value, RG_matrix_index] = int(RG_record_matrix_cell + 1)
+
+            # RB
+            # RB_matrix_index = int(str(red_blue_contrast_mean)[2])
+            # print('red_blue_contrast_mean',red_blue_contrast_mean)
+            # RB_matrix_index = int(red_blue_contrast_mean*10)
+            # print('RB_matrix_index: ',RB_matrix_index)
+            RB_matrix_index = contrast_index(red_blue_contrast_mean)
+
+            # extract origianl values
+            RB_matrix_cell = RB_matrix[blue_value, red_value, RB_matrix_index]
+            RB_record_matrix_cell = RB_record_matrix[blue_value, red_value, RB_matrix_index]
+            # update value
+            RB_matrix[blue_value, red_value, RB_matrix_index] = int(
+                (RB_matrix_cell * RB_record_matrix_cell + green_value) / (RB_record_matrix_cell + 1))
+            RB_record_matrix[blue_value, red_value, RB_matrix_index] = int(RB_record_matrix_cell + 1)
+
+            # GB
+            # GB_matrix_index = int(str(green_blue_contrast_mean)[2])
+            # GB_matrix_index = int(green_blue_contrast_mean*10)
+            GB_matrix_index = contrast_index(green_blue_contrast_mean)
+
+            # extract origianl values
+            GB_matrix_cell = GB_matrix[blue_value, green_value, GB_matrix_index]
+            GB_record_matrix_cell = GB_record_matrix[blue_value, green_value, GB_matrix_index]
+            # update value
+            GB_matrix[blue_value, green_value, GB_matrix_index] = int(
+                (GB_matrix_cell * GB_record_matrix_cell + red_value) / (GB_record_matrix_cell + 1))
+            GB_record_matrix[blue_value, green_value, GB_matrix_index] = int(GB_record_matrix_cell + 1)
+
+            num_point += 1
+
+    return num_point, RG_matrix, RG_record_matrix, RB_matrix, RB_record_matrix, GB_matrix, GB_record_matrix
